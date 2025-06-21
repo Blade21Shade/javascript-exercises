acf82c426cd121400c8697ee857da041e36a95d2
Blade21Shade <blade21shade@gmail.com>
2025-06-21 15:11:49 -0500

    07 Reverse String solution
    reverseString.js
    - Loop used to create logic
    - 2 types of error handling at the beginning
     - Empty strings, return ''
     - toReverse isn't a string, return nothing
    
    reverseString.spec.js
    - Remove skips

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,22 @@
-const reverseString = function() {
+const reverseString = function(toReverse) {
+    // Edge case and error handling
+    if (toReverse === '') {
+        return '';
+    }
 
+    if (typeof toReverse !== 'string') {
+        return;
+    }
+
+    // Core logic
+    let reversed = "";
+    let splitString = toReverse.split('');
+    
+    for (let i = splitString.length - 1; i >= 0; i--) {
+        reversed += splitString[i];
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line
