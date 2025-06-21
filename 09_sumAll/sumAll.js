cb2ac8a1a9458457487ac49aece85e58b52ee669
Blade21Shade <blade21shade@gmail.com>
2025-06-21 16:02:21 -0500

    09 Sum all solution
    sumAll.js
    - A loop does the core logic
    - The majority of this problem has to do  with error handling
     - See small rant at the end for details
    
     sumAll.spec.js
    - Remove skips
    
    Small rant
     - I have a small gripe with the amount of error handling because the ReadMe makes it sound like you are only expected to make a function that deals with 2 positive integers
     - From what the ReadMe says, one could intuitively check if the first number is greater than the second, which I did before running the first test, but the other tests go beyond the scope of what was written
     - One could argue that going beyond the scope should be expected and tested for, and that the "real world" won't tell you exactly what to expect, which are fair points
     - However, I feel like if the read me used the phrase "expects two positive integers and sums the numbers between them inclusively" (or something along those lines) it would be better for future Odin learners as it better sets up expectations that they need to test beyond just 2 positive numbers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
 
+    if (a % 1 !== 0 || b % 1 !== 0) {
+        return "ERROR";
+    }
+
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return "ERROR";
+    }
+    
+    if (a > b) {
+        let temp = a;
+        let temp2 = b;
+        b = temp;
+        a = temp2;
+    }
+    
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
