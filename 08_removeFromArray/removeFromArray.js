c2fa2fce495a7f3693d91a09ba8e58bfb6ef25ab
Blade21Shade <blade21shade@gmail.com>
2025-06-21 15:35:29 -0500

    08 Remove from array solution
    removeFromArray.js
    - Use a loop and internal for of loop to get solution
     - I looked up the rest parameter with the link Odin provided, and using it is pretty easy in this problem
    - I forgot that splice would mess with the array I was working with and didn't originally include the i--, but on one of the websites I remembered seeing that code  so I put it in, and voila, the code was fixed
    
    removeFromArray.spec.js
    - Remove skips

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    for (let i = 0; i < arr.length; i++) {
+        for (tr of toRemove) {
+            if (arr[i] === tr) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
