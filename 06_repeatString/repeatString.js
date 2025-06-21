973afe08dd546b400a08563f808fbd2a943a6dac
Blade21Shade <blade21shade@gmail.com>
2025-06-21 15:01:19 -0500

    06 Repeat String  solution
    repeatString.js
    - Add functionality with a for loop
     - Also an initial if statement to see if the given amount is less than 0
     - I was not expecting more thorough testing so I didn't do this at first; I'll attempt to be more diligent from now on
    
     repeatString.spec.js
     - Remove skips

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(toRepeat, amount) {
+    if (amount < 0) {
+        return "ERROR";
+    }
+    
+    let toReturn = "";
+    
+    for (let i = 0; i < amount; i++) {
+        toReturn += toRepeat;
+    }
+    return toReturn;
 };
 
 // Do not edit below this line
