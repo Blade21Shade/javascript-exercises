52bcea32bb563d2d77224cdca3ab2a67a0613a2d
Blade21Shade <blade21shade@gmail.com>
2025-06-21 16:18:32 -0500

    10 Leap years solution
    leapYears.js
    - Create a boolean which will be returned
    - 4 year checking logic separated from x00 year checking
     - This was enabled by changing to the boolean return rather than raw returns
     - Raw returns could still work, but this is consistent which is probably better for irl work
    - This was my original idea before writing out my erroneous code, so I guess I should've stuck with my gut
    
    leapYears.spec.js
    - Get rid of skips

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,12 +1,19 @@
 const leapYears = function(year) {
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                return true;
-            }
+    let isLeapYear = false;
+
+    if (year % 4 === 0) {
+        isLeapYear = true;
+    }
+    
+    if (isLeapYear && year % 100 === 0) {
+        if (year % 400 === 0) {
+            isLeapYear = true;
+        } else {
+            isLeapYear = false
         }
     }
-    return false;
+    
+    return isLeapYear;
 };
 
 // Do not edit below this line
