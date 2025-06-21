d7cd15c198f3d61ecb12056af7006fb6c19f369f
Blade21Shade <blade21shade@gmail.com>
2025-06-21 16:29:15 -0500

    11 Temp conversions
    tempConversions.js
    - Plugged formulas from Wiki in here
    - The functions use different rounding techniques
     - toCelsius uses Math.round() which seems to be more common
     - toFahrenheit uses a string function, toFixed, and then parses that result; Google AI brought up this one and I thought it was interesting, so I figured I'd see if I could use it too
    
     tempConversion.spec.js
     - Remove skips

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inF) {
+  let inC = (inF-32) * 5/9.0;
+  inC = Math.round(inC * 10) / 10;
+  return inC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inC) {
+  let inF = inC*(9/5.0) + 32;
+  inF = parseFloat(inF.toFixed(1));
+  return inF;
 };
 
 // Do not edit below this line
