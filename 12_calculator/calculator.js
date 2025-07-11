d186694a41ea44cc0c59d138a4dacf3842f1b3a7
Blade21Shade <blade21shade@gmail.com>
2025-07-11 11:17:58 -0500

    12 Calculator solution
    calculator.js
    - Add in functionality for each function
    
    calculator.spec.js
    - Get rid of all .skip

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,60 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let toReturn = 0;
+  for (let num of arr) {
+    toReturn += num;
+  }
+  return toReturn;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let toReturn = 1;
+  for (let num of arr) {
+    toReturn *= num;
+  }
+  return toReturn;
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+	if (pow == 0) {
+    return 1;
+  }
+
+  if (pow < 0) { // This would actually divide, but I'm assuming that isn't desired for this project
+    return base;
+  }
+  
+  let toReturn = base;
+
+  for (let i = 1; i < pow; i++) {
+    toReturn *= base;
+  }
+
+  return toReturn;
 };
 
-const factorial = function() {
-	
+const factorial = function(base) {
+  if (base < 0) {
+    return -1;
+  } else if (base == 0) {
+    return 1;
+  } else if (base == 1) {
+    return 1;
+  }
+
+  let toReturn = 1;
+  for (let i = 2; i <= base; i++) {
+    toReturn *= i;
+  }
+
+  return toReturn;
 };
 
 // Do not edit below this line
