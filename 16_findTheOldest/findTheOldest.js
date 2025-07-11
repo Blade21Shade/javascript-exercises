254a96d110d19a96e185fd45619faa4cfc4e3ee9
Blade21Shade <blade21shade@gmail.com>
2025-07-11 14:29:46 -0500

    Make solution for other tests
    findTheOldest.js
    - Uses the Date class/object from JS to make the year of death the current year if yearOfDeath is undefined when returned from a person object
    
    findTheOldest.spec.js
    - Get rid of .skip

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -5,6 +5,10 @@ const findTheOldest = function(people) {
     let ages = people.map((person) => {
         let yob = person.yearOfBirth;
         let yod = person.yearOfDeath;
+        if (yod === undefined) {
+            let temp = new Date();
+            yod = temp.getFullYear();
+        }
         let dif = yod-yob;
         if (dif > oldestAge) {
             oldestAge = dif;
