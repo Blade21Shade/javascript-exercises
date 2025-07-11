72000d0a94f443c7b39a784acbe427b6f279ca50
Blade21Shade <blade21shade@gmail.com>
2025-07-11 13:58:25 -0500

    14 Fibonacci solution
    fibonacci.js
    - Logic for solution
    
    fibonacci.spec.js
    - Get rid of .skip

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    if (member == 0) {
+        return 0;
+    }
 
+    if (member < 0) {
+        return "OOPS";
+    }
+    
+    if (member < 3) { // 1 and 2 are 1
+        return 1;
+    }
+
+    return recursiveFib(member, 2, 1, 1);
 };
+
+function recursiveFib(member, count, current, prev) {
+    let temp = current + prev;
+    count++;
+    if (count != member) {
+        return recursiveFib(member, count, temp, current);
+    }
+    
+    return temp;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
