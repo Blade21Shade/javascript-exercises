7e811eec32057b427215ea2105774acb46904853
Blade21Shade <blade21shade@gmail.com>
2025-07-11 14:19:33 -0500

    16 Find the oldest first pass
    I wanted to get the first pass done first to make sure I was referencing object properties correctly
    
    findTheOldest.js
    - make the logic for finding the oldest person (1st test)

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = {};
+    let oldestAge = 0;
+    
+    let ages = people.map((person) => {
+        let yob = person.yearOfBirth;
+        let yod = person.yearOfDeath;
+        let dif = yod-yob;
+        if (dif > oldestAge) {
+            oldestAge = dif;
+            oldestPerson = person;
+        }
+    })
 
+    return oldestPerson;
 };
 
 // Do not edit below this line
